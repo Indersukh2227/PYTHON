60e3dce9606a384efafb45fbce473d9219fafffd
Indersukh2227 <sukhjot20042@gmail.com>
2024-09-03 17:37:06 -0400

    Add files via upload

---
+++ Ex1_Output.py
@@ -0,0 +1,8 @@
+# Intro Examples
+# using print() function to display
+
+print("Hello World")
+print("Hello python World")
+print("Hello ET574")
+print("Hello sukhjot")
+print("Hi my name is sukhjot and this is my second semester in this college.I made many friends here, i am so glad that I came here and now i am persuing my studies in computer")
