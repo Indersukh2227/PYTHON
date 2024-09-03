8358e8fa4e29272e96d15cfb0a2136e4fedccb51
Indersukh2227 <sukhjot20042@gmail.com>
2024-09-03 17:46:01 -0400

    Add files via upload

---
+++ ex 2 new code.py
@@ -0,0 +1,6 @@
+# Intro Examples
+# using input() & print() function to display
+
+course  = input("Enter your course: ")
+print("Welcome to " + course + '!')
+print("My name is Sukhjot and i am turning 20 this year and i m new in this country thats why i miss my friends in india alot")
